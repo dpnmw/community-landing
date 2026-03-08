12b13f1e0be88fb4772da5c30d10fb7a692b5a6f
DPN MW <dpnmediaworks@gmail.com>
2026-03-08 11:49:42 -0400

    Update page_builder.rb

--- lib/community_landing/page_builder.rb
+++ lib/community_landing/page_builder.rb
@@ -77,7 +77,7 @@ module CommunityLanding
       html << "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n"
       html << "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n"
       html << "<link href=\"https://fonts.googleapis.com/css2?#{font_params}&display=swap\" rel=\"stylesheet\">\n"
-      if @s.fontawesome_enabled rescue false
+      if (@s.fontawesome_enabled rescue false)
         html << "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css\" crossorigin=\"anonymous\">\n"
       end
       html << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, viewport-fit=cover\">\n"
